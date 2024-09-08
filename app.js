8b1d6586fe72ad4429edb441016e728248e54c6f
RojasCristianE <cristianrg333@gmail.com>
2024-09-08 17:57:04 -0600

    Final version

--- app.js
+++ app.js
@@ -1,163 +1,20 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import express from "express";
-import axios from "axios";
 import TelegramBot from "node-telegram-bot-api";
-import puppeteer from "puppeteer";
-import sharp from "sharp";
 
-const { TOKEN, DOMAIN, PORT } = process.env;
+const {TOKEN} = process.env;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const IMG_ROUTE = path.join(__dirname, "public");
-const watermarkPath = path.join(IMG_ROUTE, "wm.png");
-const watermarkBuffer = await fs.promises.readFile(watermarkPath);
+const userId = 1538453625;
 
-const bot = new TelegramBot(TOKEN);
-bot.setWebHook(`${DOMAIN}/bot${TOKEN}`);
+const message =
+    "Lo siento, parece que hubo un error con tu mensaje anterior. Por qué no intentas enviar una foto de nuevo y me permites intentarlo de nuevo?.";
 
-const browser = await puppeteer.launch({
-    headless: false,
-    userDataDir: path.join(__dirname, "chrome_cache")
-});
-const [page] = await browser.pages();
-await page.setViewport({ width: 1366, height: 1000 });
-await page.goto("https://kwai-kolors-kolors-virtual-try-on.hf.space/", {
-    waitUntil: "networkidle2",
-    timeout: 0
-});
+const bot = new TelegramBot(TOKEN, { polling: false });
 
-const app = express();
-
-app.use(express.json());
-
-let processingQueue = [];
-let isProcessing = false;
-
-app.listen(PORT, () => console.log(`Express server is listening on ${PORT}`));
-
-const processImage = async (filePath, userId) => {
-    try {
-        console.log(`Procesando la imagen para el usuario ${userId}`);
-
-        const imgName = path.basename(filePath, ".jpg");
-        const garmentIndex = Math.ceil(Math.random() * 10);
-        const garmentImagePath = path.join(
-            IMG_ROUTE,
-            "clothes",
-            `${garmentIndex}.png`
-        );
-
-        await page.waitForSelector('#component-11 input[type="file"]');
-        const personImageInput = await page.$(
-            '#component-11 input[type="file"]'
-        );
-        await personImageInput.uploadFile(filePath);
-
-        await page.waitForSelector('#component-14 input[type="file"]');
-        const garmentImageInput = await page.$(
-            '#component-14 input[type="file"]'
-        );
-        await garmentImageInput.uploadFile(garmentImagePath);
-
-        let imgUploadCompleted = false;
-        let garmentUploadCompleted = false;
-        let notRunYet = true;
-
-        const responseListener = async (response) => {
-            const url = response.url();
-            if (url.includes("image.webp")) {
-                console.log("Imagen generada detectada: " + url);
-                page.off("response", responseListener);
-
-                const imageResponse = await axios({
-                    url,
-                    method: "GET",
-                    responseType: "arraybuffer",
-                });
-
-                const imageBuffer = Buffer.from(imageResponse.data);
-                const image = sharp(imageBuffer);
-                const watermark = sharp(watermarkBuffer);
-
-                const { width } = await image.metadata();
-                const input = await watermark.resize({ width }).toBuffer();
-                const finalImage = await image
-                    .composite([{ input, gravity: "south" }])
-                    .toFormat("jpg")
-                    .toBuffer();
-
-                await bot.sendPhoto(userId, finalImage, {
-                    caption: "Aquí tenés tu foto. ¡Perdón por la demora!",
-                });
-
-                const transformedImgPath = path.join(
-                    IMG_ROUTE,
-                    "transformed",
-                    `${imgName}.jpg`
-                );
-                await fs.promises.writeFile(transformedImgPath, finalImage);
-
-                await page.reload();
-
-                imgUploadCompleted = false;
-                garmentUploadCompleted = false;
-                notRunYet = true;
-
-                processNextImage();
-            }
-
-            if (!imgUploadCompleted)
-                imgUploadCompleted = url.includes(`${imgName}.jpg`);
-            if (!garmentUploadCompleted)
-                garmentUploadCompleted = url.includes(`${garmentIndex}.png`);
-
-            const bothImgUploaded =
-                imgUploadCompleted && garmentUploadCompleted;
-
-            if (bothImgUploaded && notRunYet) {
-                notRunYet = false;
-                await page.waitForSelector("#button");
-                const runButton = await page.$("#button");
-                await runButton.click();
-            }
-        };
-
-        page.on("response", responseListener);
-    } catch (error) {
-        console.error("Error procesando la imagen:", filePath);
-        processNextImage();
-    }
-};
-
-const processNextImage = () => {
-    if (processingQueue.length > 0) {
-        const { filePath, userId } = processingQueue.shift();
-        processImage(filePath, userId);
-    } else {
-        isProcessing = false;
-    }
-};
-
-const addImageToQueue = (filePath, userId) => {
-    processingQueue.push({ filePath, userId });
-
-    if (!isProcessing) {
-        isProcessing = true;
-        processNextImage();
-    }
-};
-
-const processPendingImages = () => {
-    fs.readdir(path.join(IMG_ROUTE, "users"), (err, files) => {
-        files.forEach((file) => {
-            const userId = file.split("_")[0];
-            const filePath = path.join(IMG_ROUTE, "users", file);
-            addImageToQueue(filePath, userId);
-        });
+bot.sendMessage(userId, message)
+    .then(() => {
+        console.log(`Mensaje enviado al usuario ${userId}`);
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.error("Error al enviar el mensaje:", error);
+        process.exit(1);
     });
-};
-
-processPendingImages();
--- main.js
+++ main.js
@@ -18,11 +18,7 @@ const watermarkBuffer = await fs.promises.readFile(watermarkPath);
 const bot = new TelegramBot(TOKEN);
 bot.setWebHook(`${DOMAIN}/bot${TOKEN}`);
 
-const browser = await puppeteer.launch({
-    headless: false,
-    userDataDir: path.join(__dirname, "chrome_cache"),
-    devtools: true,
-});
+const browser = await puppeteer.launch({userDataDir: path.join(__dirname, "chrome_cache")});
 const [page] = await browser.pages();
 await page.setViewport({ width: 1366, height: 800 });
 await page.goto("https://kwai-kolors-kolors-virtual-try-on.hf.space/", {
@@ -38,22 +34,10 @@ let isProcessing = false;
 let lastErrorTime = 0;
 let WAIT_TIME = 600000;
 
-app.post(`/bot${TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
+app.post(`/bot${TOKEN}`, ({body}, res) => {
+	bot.processUpdate(body);
+	res.sendStatus(200)
 });
-
-app.get(
-    "/public/:subfolder/:filename",
-    ({ params: { subfolder, filename } }, res) => {
-        const filePath = path.join(IMG_ROUTE, subfolder, filename);
-
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-            if (err) res.status(404).json({ error: "File not found" });
-            else res.sendFile(filePath);
-        });
-    }
-);
 
 app.listen(PORT, () => console.log(`Express server is listening on ${PORT}`));
 
@@ -151,8 +135,29 @@ bot.on(
                 const responseListener = async (response) => {
                     const url = response.url();
 
-                    if (url.includes("image.webp")) {
-                        console.log("Imagen generada detectada: " + url);
+					if (!imgUploadCompleted) {
+                        imgUploadCompleted =
+                            imgUploadCompleted ||
+                            url.includes(`${imgName}.jpg`);
+					}
+
+                    if (!garmentUploadCompleted) {
+						garmentUploadCompleted =
+							garmentUploadCompleted || url.includes(`${i}.png`);
+					}
+
+                    const bothImgUploaded =
+                        imgUploadCompleted && garmentUploadCompleted;
+
+                    if (bothImgUploaded && notRunYet) {
+                        notRunYet = false;
+
+                        await page.waitForSelector("#button");
+                        const runButton = await page.$("#button");
+                        await runButton.click();
+                    }
+
+					if (url.includes("image.webp")) {
                         page.off("response", responseListener);
                         console.log("Listener de respuestas desactivado.");
 
@@ -194,43 +199,11 @@ bot.on(
 
                         await page.reload();
 
-						imgUploadCompleted = false;
-						garmentUploadCompleted = false;
+                        imgUploadCompleted = false;
+                        garmentUploadCompleted = false;
+						notRunYet = true;
 
                         isProcessing = false;
-                    }
-
-					if (!imgUploadCompleted) {
-						console.log(
-                            `Comprobando si la petición ${url} es sobre la imagen generada...`
-                        );
-                        console.log("Estado actual", imgUploadCompleted);
-                        imgUploadCompleted =
-                            imgUploadCompleted ||
-                            url.includes(`${imgName}.jpg`);
-                        console.log("Nuevo estado",imgUploadCompleted);
-					}
-
-                    if (!garmentUploadCompleted) {
-						console.log(
-							`Comprobando si la petición ${url} es sobre la imagen de la vestimenta...`
-						);
-						console.log("Estado actual", garmentUploadCompleted);
-						garmentUploadCompleted =
-							garmentUploadCompleted || url.includes(`${i}.png`);
-						console.log("Nuevo estado", garmentUploadCompleted);
-					}
-
-                    const bothImgUploaded =
-                        imgUploadCompleted && garmentUploadCompleted;
-
-                    if (bothImgUploaded && notRunYet) {
-                        notRunYet = false;
-                        console.log("Ambas imágenes han terminado de cargar.");
-
-                        await page.waitForSelector("#button");
-                        const runButton = await page.$("#button");
-                        await runButton.click();
                     }
                 };
 
